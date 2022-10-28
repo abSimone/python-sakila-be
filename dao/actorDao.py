a8ec9b1448c8c54355405dc91c8dc96865be7c50
abSimone <s.angelonibei@digitality-consulting.com>
2022-10-28 12:23:29 +0200

    refactor select

--- dao/actorDao.py
+++ dao/actorDao.py
@@ -6,7 +6,10 @@ class ActorDao:
     @classmethod
     def findAllActors(cls):
         MySql.openConnection()
-        MySql.query("""SELECT first_name, last_name FROM Actor LIMIT 10""")
+        MySql.query(
+          "SELECT first_name, last_name \
+           FROM Actor LIMIT 10"
+          )
         data = MySql.getResults()
         MySql.closeConnection()
         return data
@@ -14,10 +17,12 @@ class ActorDao:
     @classmethod
     def findFirstNameAndLastnameByFilmTitle(cls, titolo_film):
         MySql.openConnection()
-        MySql.query(f"SELECT a.first_name, a.last_name \
-                      FROM Actor a, Film f, Film_Actor fa \
-                      WHERE a.actor_id = fa.actor_id AND fa.film_id = f.film_id \
-                      AND f.title = '{titolo_film}'")
+        MySql.query(
+          f"SELECT a.first_name, a.last_name \
+            FROM Actor a, Film f, Film_Actor fa \
+            WHERE a.actor_id = fa.actor_id AND fa.film_id = f.film_id \
+            AND f.title = '{titolo_film}'"
+            )
         data = MySql.getResults()
         MySql.closeConnection()
         return data
