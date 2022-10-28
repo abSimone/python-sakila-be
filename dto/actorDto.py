056fd7e304a2e6a26fb311b44b6df2271a0fecb7
abSimone <s.angelonibei@digitality-consulting.com>
2022-10-28 14:41:26 +0200

    CORS management

--- dto/actorDto.py
+++ dto/actorDto.py
@@ -2,25 +2,25 @@ from dao.actorDao import ActorDao
 
 
 class Actor:
-  def __init__(self, nome, cognome):
-    self.nome = nome
-    self.cognome = cognome
+    def __init__(self, nome, cognome):
+        self.nome = nome
+        self.cognome = cognome
 
 
 class ActorDto:
 
-  @classmethod
-  def getAllActors(cls):
-    data = ActorDao.findAllActors()
-    newDict = {}
-    for i, lista in enumerate(data):
-      newDict[i] = Actor(lista[0], lista[1])
-    return newDict
+    @classmethod
+    def getAllActors(cls):
+        data = ActorDao.findAllActors()
+        newList = []
+        for lista in data:
+            newList.append(Actor(lista[0], lista[1]))
+        return newList
 
-  @classmethod
-  def getActorsForFilm(cls, titolo_film: str):
-    data = ActorDao.findFirstNameAndLastnameByFilmTitle(titolo_film)
-    newDict = {}
-    for i, lista in enumerate(data):
-      newDict[i] = Actor(lista[0], lista[1])
-    return newDict
+    @classmethod
+    def getActorsForFilm(cls, titolo_film: str):
+        data = ActorDao.findFirstNameAndLastnameByFilmTitle(titolo_film)
+        newList = []
+        for lista in data:
+            newList.append(Actor(lista[0], lista[1]))
+        return newList
