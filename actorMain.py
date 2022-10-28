4af0eb2ceedc455f379b7cd34db7d4a0d1b4a3e7
abSimone <s.angelonibei@digitality-consulting.com>
2022-10-28 14:48:51 +0200

    regex origin

--- actorMain.py
+++ actorMain.py
@@ -4,23 +4,20 @@ from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
-origins = [
-    "http://localhost:4200",   
-]
-
 app.add_middleware(
     CORSMiddleware,
-  
     allow_origin_regex="http://.*",
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
 
+
 @app.get('/')
 def getAllActors():
-  return {"actors" : ActorDto.getAllActors()}
+    return {"actors": ActorDto.getAllActors()}
+
 
 @app.get('/{titolo_film}')
-def getActorsForFilm(titolo_film:str):
-  return {"actors" : ActorDto.getActorsForFilm(titolo_film)}
+def getActorsForFilm(titolo_film: str):
+    return {"actors": ActorDto.getActorsForFilm(titolo_film)}
