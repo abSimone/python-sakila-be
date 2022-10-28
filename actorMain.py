d4bc75d06be14aef2e7cad5bf6e87d20c265a60f
abSimone <s.angelonibei@digitality-consulting.com>
2022-10-28 14:52:30 +0200

    errore fixato

--- actorMain.py
+++ actorMain.py
@@ -12,12 +12,10 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-
 @app.get('/')
 def getAllActors():
-    return {"actors": ActorDto.getAllActors()}
-
+  return {"actors" : ActorDto.getAllActors()}
 
 @app.get('/{titolo_film}')
-def getActorsForFilm(titolo_film: str):
-    return {"actors": ActorDto.getActorsForFilm(titolo_film)}
+def getActorsForFilm(titolo_film:str):
+  return {"actors" : ActorDto.getActorsForFilm(titolo_film)}
