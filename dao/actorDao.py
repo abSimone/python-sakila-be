883aba99b52f3dc1de3744ca13e7cf97343a3e5c
Dev1 - bosco <abosco@visioture.com>
2023-05-05 16:59:43 +0200

    task 5009

--- dao/actorDao.py
+++ dao/actorDao.py
@@ -8,12 +8,26 @@ class ActorDao:
         MySql.query(
           "SELECT first_name, last_name \
             FROM Actor\
+            WHERE first_name="alex"\  
             ORDER BY last_name"
           )
+        data1 = MySql.getResults()
+        MySql.closeConnection()
+        return data1
+@classmethod
+def findFirstNameAndLastnameByFilmTitle2(cls, titolo_film):
+        MySql.openConnection()
+        MySql.query(
+          f"SELECT a.first_name, a.last_name \
+            FROM Actor a, Film f, Film_Actor fa \
+            WHERE a.actor_id = fa.actor_id AND fa.film_id = f.film_id \
+            AND f.title = '{titolo_film}'"
+            )
         data = MySql.getResults()
         MySql.closeConnection()
         return data
-
+      
+    
     @classmethod
     def findFirstNameAndLastnameByFilmTitle(cls, titolo_film):
         MySql.openConnection()
